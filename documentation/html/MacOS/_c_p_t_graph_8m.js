55be2ba515d0fcfe61692c0739477619f6e9576f
Alexander List <alist@listprojects.com>
2013-03-14 18:10:23 -0400

    bye bye cp-bs

--- documentation/html/MacOS/_c_p_t_graph_8m.js
+++
@@ -1,4 +0,0 @@
-var _c_p_t_graph_8m =
-[
-    [ "CPTGraphNeedsRedrawNotification", "_c_p_t_graph_8m.html#gacdcc79d94fa2215225c836c3b2f04565", null ]
-];
--- documentation/html/MacOS/category_c_p_t_layer_07_c_p_t_platform_specific_layer_extensions_08.js
+++
@@ -1,4 +0,0 @@
-var category_c_p_t_layer_07_c_p_t_platform_specific_layer_extensions_08 =
-[
-    [ "imageOfLayer", "category_c_p_t_layer_07_c_p_t_platform_specific_layer_extensions_08.html#a8090476a4b0d5ce1ef54803af81e81d1", null ]
-];
